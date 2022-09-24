a370d58c6bbdc584f8554eb6ac684a1e07d1c5bd
MaCeleste <mceleste.orona@gmail.com>
2022-09-24 23:20:22 +0100

    Partial solution that will accept one argument, the problems. It will not display results. 8/10 tests passed.

--- ArithmeticFormatter.py
+++ ArithmeticFormatter.py
@@ -1,6 +1,6 @@
-verifiedProblems = []
-
 def arithmetic_arranger(problems):
+  problemsAsInt = []
+  problemsAsStr = []
   for problem in problems:
 
     #checking if up to 5 problems were entered
@@ -19,13 +19,59 @@ def arithmetic_arranger(problems):
       break
     #checking if operands contain only digits
     try:
-      int(operands[0])
-      int(operands[2])
+      operands[0] = int(operands[0])
+      operands[2] = int(operands[2])
     except:
-      return('Numbers must only contain digits.')
+      return('Error: Numbers must only contain digits.')
       break
-    verifiedProblems.append(operands)
-  print(verifiedProblems)
+    #creating a list that stores the problems with the operands converted to integers
+    problemsAsInt.append(operands)
+    #splitting the user input into strings
+    operandsAsStr = problem.split()
+    problemsAsStr.append(operandsAsStr)
+
+  #in this variable I will store the problems with the correct number of spaces
+  problemsWithSpaces = []
+  #iterating through each problem to find the longest of the two operands and
+  #apply the correct number of spaces
+  for sproblem in problemsAsStr:
+    problemWithSpaces = []
+    longest = max(sproblem, key=len)
+    #print(longest)
+    if longest == sproblem[2]:
+      smallest = sproblem[0].rjust(len(longest)+2, ' ')
+      problemWithSpaces.append(smallest)
+      problemWithSpaces.append(sproblem[1]+" "+longest)
+    if longest == sproblem[0]:
+      smallest = sproblem[2].rjust(len(longest), ' ')
+      problemWithSpaces.append('  '+longest)
+      problemWithSpaces.append(sproblem[1]+" "+smallest)
+
+    problemsWithSpaces.append(problemWithSpaces)
+
+
+  #to put each the first operand of each problem in the first line,
+  #the second operand of each problem in the second line
+  #and add a third line with the correct number of '-'
+  firstLine = ''
+  secondLine = ''
+  thirdLine = ''
+  count = 1
+  for fproblem in problemsWithSpaces:
+    if count == len(problemsWithSpaces):
+      firstLine = firstLine + fproblem[0]
+      secondLine = secondLine + fproblem[1]
+      thirdLine = thirdLine + '-'*len(fproblem[0])
+    else:
+      firstLine = firstLine + fproblem[0] + '    '
+      secondLine = secondLine + fproblem[1] + '    '
+      thirdLine = thirdLine + '-'*len(fproblem[0]) + '    '
+    count += 1
+
+  #concatenate the three lines
+  arranged_problems = firstLine + '\n' + secondLine + '\n' + thirdLine
+
+  return arranged_problems
 
 
 
